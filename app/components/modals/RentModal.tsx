69953bc619642a81ac7335e97b8d086db2a80974
Marina Sheigets <marinasejgec34@gmail.com>
2023-10-10 18:10:17 +0300

    feat: add image uploader

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect, { CountrySelectValue } from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 enum STEPS {
 	CATEGORY = 0,
 	LOCATION = 1,
@@ -45,7 +46,7 @@ function RentModal() {
 	const guestCount = watch('guestCount');
 	const roomCount = watch('roomCount');
 	const bathroomCount = watch('bathroomCount');
-
+	const imageSrc = watch('imageSrc');
 	const Map = useMemo(
 		() =>
 			dynamic(() => import('../Map'), {
@@ -145,6 +146,21 @@ function RentModal() {
 					subtitle='How many bathrooms do you have?'
 					value={bathroomCount}
 					onChange={(value) => setCustomValue('bathroomCount', value)}
+				/>
+			</div>
+		);
+	}
+
+	if (step === STEPS.IMAGES) {
+		bodyContent = (
+			<div className='flex flex-col gap-8'>
+				<Heading
+					title='Add a photo of your place'
+					subtitle='Show guests what your place looks like'
+				/>
+				<ImageUpload
+					onChange={(value) => setCustomValue('imageSrc', value)}
+					value={imageSrc}
 				/>
 			</div>
 		);
