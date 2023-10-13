75aa2c67e2765df66fbcc67849bcece6d037d38b
Marina Sheigets <marinasejgec34@gmail.com>
2023-10-13 23:25:52 +0300

    feat: add default loading and error components

---
+++ app/error.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useEffect } from 'react';
+import EmptyState from './components/EmptyState';
+
+interface ErrorProps {
+	error: Error;
+}
+
+const Error = ({ error }: ErrorProps) => {
+	useEffect(() => {
+		console.error(error);
+	}, [error]);
+	return <EmptyState title='Oops' subtitle='Something went wrong' />;
+};
+
+export default Error;
